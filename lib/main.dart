ab4d28f6943ce6944f1c9857801b8da34d579467
erfan <erfan01724@gmail.com>
2024-02-27 19:44:55 +0330

    My Shop stated!

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:digikala/screens/HomeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
